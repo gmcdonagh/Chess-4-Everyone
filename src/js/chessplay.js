f3826b666f7510a4f8add2052ff69c36d77f44f2
gmcdonagh <gregmcd@live.unc.edu>
2017-03-29 16:48:26 -0400

    Update chessplay.js

--- src/js/chessplay.js
+++ src/js/chessplay.js
@@ -1,61 +1,3 @@
-<<<<<<< HEAD
-function executeGame(playerSetting, difficultySetting) {
-    // do not pick up pieces if the game is over
-    // only pick up pieces for White
-    var board;
-
-    var onDragStart = function(source, piece, position, orientation) {
-      if (game.in_checkmate() === true || game.in_draw() === true ||
-        piece.search(/^b/) !== -1) {
-        return false;
-      }
-    };
-
-    var makeRandomMove = function() {
-      var possibleMoves = game.moves();
-
-      // game over
-      if (possibleMoves.length === 0) return;
-
-      var randomIndex = Math.floor(Math.random() * possibleMoves.length);
-      game.move(possibleMoves[randomIndex]);
-      board.position(game.fen());
-    };
-
-    var onDrop = function(source, target) {
-      // see if the move is legal
-      var move = game.move({
-        from: source,
-        to: target,
-        promotion: 'q' // NOTE: always promote to a queen for example simplicity
-      });
-
-      // illegal move
-      if (move === null) return 'snapback';
-
-      // make random legal move for black
-      window.setTimeout(makeRandomMove, 250);
-    };
-
-    // update the board position after the piece snap
-    // for castling, en passant, pawn promotion
-    var onSnapEnd = function() {
-      board.position(game.fen());
-    };
-
-    var cfg = {
-      draggable: true,
-      position: 'start',
-      onDragStart: onDragStart,
-      onDrop: onDrop,
-      onSnapEnd: onSnapEnd
-    };
-
-    var game = new Chess();
-
-    board = ChessBoard('board', cfg);
-    alert("D: " + playerSetting + " P: " + difficultySetting);
-=======
 var Chess = function(fen) {
 
   /* jshint indent: false */
@@ -2679,7 +2621,3 @@ function executeSettings() {
 $(document).ready(function () {
     executeMainMenu(0,0);
 });
-<<<<<<< HEAD
-
-=======
->>>>>>> 629e0349ac917337d38ebbe1822560b054d3707c
