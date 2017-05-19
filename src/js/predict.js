bf0c1c3b2fc82b601d3ad00242da220dc52a871c
gmcdonagh <gregmcd@live.unc.edu>
2017-05-19 17:47:41 -0400

    Update predict.js

--- src/js/predict.js
+++ src/js/predict.js
@@ -12,6 +12,163 @@ var g_cellSize = 45;
 var finalData = "default";
 var haschanged = 0;
 
+function UINewGame() {
+    moveNumber = 1;
+
+    var pgnTextBox = document.getElementById("PgnTextBox");
+    pgnTextBox.value = "";
+
+    EnsureAnalysisStopped();
+    ResetGame();
+    if (InitializeBackgroundEngine()) {
+        g_backgroundEngine.postMessage("go");
+    }
+    g_allMoves = [];
+    RedrawBoard();
+
+    if (!g_playerWhite) {
+        SearchAndRedraw();
+    }
+}
+
+function EnsureAnalysisStopped() {
+    if (g_analyzing && g_backgroundEngine != null) {
+        g_backgroundEngine.terminate();
+        g_backgroundEngine = null;
+    }
+}
+
+function UIAnalyzeToggle() {
+    if (InitializeBackgroundEngine()) {
+        if (!g_analyzing) {
+            g_backgroundEngine.postMessage("analyze");
+        } else {
+            EnsureAnalysisStopped();
+        }
+        g_analyzing = !g_analyzing;
+        document.getElementById("AnalysisToggleLink").innerText = g_analyzing ? "Analysis: On" : "Analysis: Off";
+    } else {
+        alert("Your browser must support web workers for analysis - (chrome4, ff4, safari)");
+    }
+}
+
+function UIChangeFEN() {
+    if (!g_changingFen) {
+        var fenTextBox = document.getElementById("FenTextBox");
+        var result = InitializeFromFen(fenTextBox.value);
+        if (result.length != 0) {
+            //alert(result);
+            UpdatePVDisplay(result);
+            return;
+        } else {
+            UpdatePVDisplay('');
+        }
+        g_allMoves = [];
+
+        EnsureAnalysisStopped();
+        InitializeBackgroundEngine();
+
+        g_playerWhite = !!g_toMove;
+        g_backgroundEngine.postMessage("position " + GetFen());
+
+        RedrawBoard();
+    }
+}
+
+function UIChangeStartPlayer() {
+    g_playerWhite = !g_playerWhite;
+    RedrawBoard();
+}
+
+function UpdatePgnTextBox(move) {
+    var pgnTextBox = document.getElementById("PgnTextBox");
+    if (g_toMove != 0) {
+        pgnTextBox.value += moveNumber + ". ";
+        moveNumber++;
+    }
+    pgnTextBox.value += GetMoveSAN(move) + " ";
+}
+
+function UIChangeTimePerMove() {
+    var timePerMove = document.getElementById("TimePerMove");
+    g_timeout = parseInt(timePerMove.value, 10);
+}
+
+function FinishMove(bestMove, value, timeTaken, ply) {
+
+    if (bestMove != null) {
+
+        UIPlayMove(bestMove, BuildPVMessage(bestMove, value, timeTaken, ply));
+    } else {
+        alert("Checkmate!");
+    }
+}
+
+function UIPlayMove(move, pv) {
+    UpdatePgnTextBox(move);
+
+    g_allMoves[g_allMoves.length] = move;
+    MakeMove(move);
+
+    UpdatePVDisplay(pv);
+
+    UpdateFromMove(move);
+}
+
+function UIUndoMove() {
+  if (g_allMoves.length == 0) {
+    return;
+  }
+
+  if (g_backgroundEngine != null) {
+    g_backgroundEngine.terminate();
+    g_backgroundEngine = null;
+  }
+
+  UnmakeMove(g_allMoves[g_allMoves.length - 1]);
+  g_allMoves.pop();
+
+  if (g_playerWhite != !!g_toMove && g_allMoves.length != 0) {
+    UnmakeMove(g_allMoves[g_allMoves.length - 1]);
+    g_allMoves.pop();
+  }
+
+  RedrawBoard();
+}
+
+function UpdatePVDisplay(pv) {
+
+    if (pv != null) {
+        var outputDiv = document.getElementById("output");
+        if (outputDiv.firstChild != null) {
+            outputDiv.removeChild(outputDiv.firstChild);
+        }
+        outputDiv.appendChild(document.createTextNode(pv));
+        //alert(outputDiv);
+    }
+}
+
+function SearchAndRedraw() {
+    if (g_analyzing) {
+        EnsureAnalysisStopped();
+        InitializeBackgroundEngine();
+        g_backgroundEngine.postMessage("position " + GetFen());
+        g_backgroundEngine.postMessage("analyze");
+        return;
+    }
+
+    if (InitializeBackgroundEngine()) {
+        //g_backgroundEngine.postMessage("search " + g_timeout);
+        g_backgroundEngine.postMessage("position " + "r1b1qr1k/pp5p/1bp2ppQ/3p1P2/2BpP1P1/3P1N2/PPP5/R4RK1 w - - 0 1");
+        g_backgroundEngine.postMessage("analyze");
+        //g_backgroundEngine.postMessage("search " + g_timeout);
+
+    } else {
+	Search(FinishMove, 99, null);
+
+    }
+}
+
 var g_backgroundEngineValid = true;
 var g_backgroundEngine;
 
@@ -24,8 +181,7 @@ function InitializeBackgroundEngine() {
         g_backgroundEngineValid = true;
         try {
           //alert("new worker");
-            g_backgroundEngine = new Worker("garbochess.js");
-            //alert("worker?")
+            g_backgroundEngine = new Worker("js/garbochess.js");
             g_backgroundEngine.onmessage = function (e) {
                 if (e.data.match("^pv") == "pv") {
                     set_final(e.data);
@@ -57,7 +213,204 @@ function InitializeBackgroundEngine() {
     return g_backgroundEngineValid;
 }
 
+function UpdateFromMove(move) {
+    var fromX = (move & 0xF) - 4;
+    var fromY = ((move >> 4) & 0xF) - 2;
+    var toX = ((move >> 8) & 0xF) - 4;
+    var toY = ((move >> 12) & 0xF) - 2;
 
+    if (!g_playerWhite) {
+        fromY = 7 - fromY;
+        toY = 7 - toY;
+        fromX = 7 - fromX;
+        toX = 7 - toX;
+    }
+
+    if ((move & moveflagCastleKing) ||
+        (move & moveflagCastleQueen) ||
+        (move & moveflagEPC) ||
+        (move & moveflagPromotion)) {
+        RedrawPieces();
+    } else {
+        var fromSquare = g_uiBoard[fromY * 8 + fromX];
+        $(g_uiBoard[toY * 8 + toX])
+            .empty()
+            .append($(fromSquare).children());
+    }
+}
+
+function RedrawPieces() {
+    for (y = 0; y < 8; ++y) {
+        for (x = 0; x < 8; ++x) {
+            var td = g_uiBoard[y * 8 + x];
+            var pieceY = g_playerWhite ? y : 7 - y;
+            var piece = g_board[((pieceY + 2) * 0x10) + (g_playerWhite ? x : 7 - x) + 4];
+            var pieceName = null;
+            switch (piece & 0x7) {
+                case piecePawn: pieceName = "pawn"; break;
+                case pieceKnight: pieceName = "knight"; break;
+                case pieceBishop: pieceName = "bishop"; break;
+                case pieceRook: pieceName = "rook"; break;
+                case pieceQueen: pieceName = "queen"; break;
+                case pieceKing: pieceName = "king"; break;
+            }
+            if (pieceName != null) {
+                pieceName += "_";
+                pieceName += (piece & 0x8) ? "white" : "black";
+            }
+
+            if (pieceName != null) {
+                var img = document.createElement("div");
+                $(img).addClass('sprite-' + pieceName);
+                img.style.backgroundImage = "url('img/sprites.png')";
+                img.width = g_cellSize;
+                img.height = g_cellSize;
+                var divimg = document.createElement("div");
+                divimg.appendChild(img);
+
+                $(divimg).draggable({ start: function (e, ui) {
+                    if (g_selectedPiece === null) {
+                        g_selectedPiece = this;
+                        var offset = $(this).closest('table').offset();
+                        g_startOffset = {
+                            left: e.pageX - offset.left,
+                            top: e.pageY - offset.top
+                        };
+                    } else {
+                        return g_selectedPiece == this;
+                    }
+                }});
+
+                $(divimg).mousedown(function(e) {
+                    if (g_selectedPiece === null) {
+                        var offset = $(this).closest('table').offset();
+                        g_startOffset = {
+                            left: e.pageX - offset.left,
+                            top: e.pageY - offset.top
+                        };
+                        e.stopPropagation();
+                        g_selectedPiece = this;
+                        g_selectedPiece.style.backgroundImage = "url('img/transpBlue50.png')";
+                    } else if (g_selectedPiece === this) {
+                        g_selectedPiece.style.backgroundImage = null;
+                        g_selectedPiece = null;
+                    }
+                });
+
+                $(td).empty().append(divimg);
+            } else {
+                $(td).empty();
+            }
+        }
+    }
+}
+
+function RedrawBoard() {
+    var div = $("#board")[0];
+
+    var table = document.createElement("table");
+    table.cellPadding = "0px";
+    table.cellSpacing = "0px";
+    $(table).addClass('no-highlight');
+
+    var tbody = document.createElement("tbody");
+
+    g_uiBoard = [];
+
+    var dropPiece = function (e, ui) {
+        var endX = e.pageX - $(table).offset().left;
+        var endY = e.pageY - $(table).offset().top;
+
+        endX = Math.floor(endX / g_cellSize);
+        endY = Math.floor(endY / g_cellSize);
+
+        var startX = Math.floor(g_startOffset.left / g_cellSize);
+        var startY = Math.floor(g_startOffset.top / g_cellSize);
+
+        if (!g_playerWhite) {
+            startY = 7 - startY;
+            endY = 7 - endY;
+            startX = 7 - startX;
+            endX = 7 - endX;
+        }
+
+        var moves = GenerateValidMoves();
+        var move = null;
+        for (var i = 0; i < moves.length; i++) {
+            if ((moves[i] & 0xFF) == MakeSquare(startY, startX) &&
+                ((moves[i] >> 8) & 0xFF) == MakeSquare(endY, endX)) {
+                move = moves[i];
+            }
+        }
+
+        if (!g_playerWhite) {
+            startY = 7 - startY;
+            endY = 7 - endY;
+            startX = 7 - startX;
+            endX = 7 - endX;
+        }
+
+        g_selectedPiece.style.left = 0;
+        g_selectedPiece.style.top = 0;
+
+        if (!(startX == endX && startY == endY) && move != null) {
+            UpdatePgnTextBox(move);
+
+            if (InitializeBackgroundEngine()) {
+                g_backgroundEngine.postMessage(FormatMove(move));
+            }
+
+            g_allMoves[g_allMoves.length] = move;
+            MakeMove(move);
+
+            UpdateFromMove(move);
+
+            g_selectedPiece.style.backgroundImage = null;
+            g_selectedPiece = null;
+
+            var fen = GetFen();
+            document.getElementById("FenTextBox").value = fen;
+
+            setTimeout("SearchAndRedraw()", 0);
+        } else {
+            g_selectedPiece.style.backgroundImage = null;
+            g_selectedPiece = null;
+        }
+    };
+
+    for (y = 0; y < 8; ++y) {
+        var tr = document.createElement("tr");
+
+        for (x = 0; x < 8; ++x) {
+            var td = document.createElement("td");
+            td.style.width = g_cellSize + "px";
+            td.style.height = g_cellSize + "px";
+            td.style.backgroundColor = ((y ^ x) & 1) ? "#D18947" : "#FFCE9E";
+            tr.appendChild(td);
+            g_uiBoard[y * 8 + x] = td;
+        }
+
+        tbody.appendChild(tr);
+    }
+
+    table.appendChild(tbody);
+
+    $('body').droppable({ drop: dropPiece });
+    $(table).mousedown(function(e) {
+        if (g_selectedPiece !== null) {
+            dropPiece(e);
+        }
+    });
+
+    RedrawPieces();
+
+    $(div).empty();
+    div.appendChild(table);
+
+    g_changingFen = true;
+    document.getElementById("FenTextBox").value = GetFen();
+    g_changingFen = false;
+}
 function set_final(new_move) {
     //alert(new_move);
     haschanged = 1;
@@ -88,7 +441,7 @@ function call_this(fen) {
       setTimeout(function(){
         //alert(get_final());
         resolve(get_final());
-      }, 1000);
+      }, 100);
       //resolve(get_final()); // fulfilled
     } else {
       var reason = new Error('mom is not happy');
